fa5443dc027dfb001ee47f7ba31bdf23030df159
JohnMoehrke <JohnMoehrke@gmail.com>
2025-03-11 13:23:58 -0500

    Initial python for creating sensitive test data

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create Data Python Script
+
+This python script will create data given an expansion valueSet. To get the expansion in the ValueSet
+
+- One method is to go to an IG that uses the ValueSet. The normal ValueSet in the IG will not have the expansions within it. BUT, there will be an expansions.json in the publication folder that is a Bundle of all of the ValueSets in the IG with expansion at the time of publication. Thus you just extract the ValueSet you need out, and use it.
+  
+This script is very simple, and uses a hardcoded patient "Patient/1"
+
+> python createData.py 10 > out.json
---
+++ createData.py
@@ -0,0 +1,57 @@
+# Given the ValueSet-SlsSensitiveSDV.json as input of sensitive code values, create N FHIR Observation resources with a random sensitive code value, a random date between 2000-01-01 and 2020-12-31, patient reference to Patient/1, and a valueString of "sensitive".
+# The output is a FHIR Bundle with N Observation resources.
+# Usage: python createData.py N
+# N is the number of Observation resources to create.
+
+import json
+import random
+import sys
+from datetime import datetime, timedelta
+
+# Load the sensitive code values from ValueSet-SlsSensitiveSDV.json
+with open('ValueSet-SlsSensitiveSDV.json') as f:
+    data = json.load(f)
+    sensitiveCodes = data['expansion']['contains']
+
+# Create N Observation resources
+N = int(sys.argv[1])
+bundle = {
+    "resourceType": "Bundle",
+    "type": "transaction",
+    "entry": []
+}
+for i in range(N):
+
+    # Randomly select a sensitive code value
+    code = random.choice(sensitiveCodes)['code']
+
+    # Randomly select a date between 2000-01-01 and 2020-12-31
+    date = datetime(2000, 1, 1) + timedelta(days=random.randint(0, 7670))
+
+    # Create an Observation resource
+    observation = {
+        "resource": {
+            "resourceType": "Observation",
+            "status": "final",
+            "code": {
+                "coding": [{
+                    "system": "http://loinc.org",
+                    "code": code
+                }]
+            },
+            "subject": {
+                "reference": "Patient/1"
+            },
+            "effectiveDateTime": date.strftime('%Y-%m-%dT%H:%M:%S'),
+            "valueString": "sensitive"
+        },
+        "request": {
+            "method": "POST",
+            "url": "Observation"
+        }
+    }
+    bundle['entry'].append(observation)
+
+# Output the FHIR Bundle
+print(json.dumps(bundle, indent=2))
+
